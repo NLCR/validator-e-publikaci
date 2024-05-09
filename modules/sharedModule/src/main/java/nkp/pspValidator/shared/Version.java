17f9cfbf8f2f7ad5b9be91994ab0e370d9c34243
Martin Řehánek <martin.rehanek@gmail.com>
2024-05-09 11:29:34 +0200

    cgange version to 2.2.1

--- modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "2.2";
+    public static String VERSION_CODE = "2.2.1";
     public static String BUILD_DATE = "9. 5. 2024";
 }
