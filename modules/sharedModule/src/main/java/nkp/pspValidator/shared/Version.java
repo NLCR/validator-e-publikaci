661630fe5932ac403ddb0e4732e990fe7d4674bf
Jan Bilwachs <jan.bilwachs@gmail.com>
2026-09-14 16:06:15 +0200

    set version 2.3 to 2.4

--- modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
+++ modules/sharedModule/src/main/java/nkp/pspValidator/shared/Version.java
@@ -7,6 +7,6 @@ public class Version {
     /**
      * Version code may contain up to one hyphen separating actual version code from suffix dev/alpha/beta, etc.
      */
-    public static String VERSION_CODE = "2.3";
-    public static String BUILD_DATE = "8. 10. 2024";
+    public static String VERSION_CODE = "2.4-beta";
+    public static String BUILD_DATE = "14. 9. 2026";
 }
